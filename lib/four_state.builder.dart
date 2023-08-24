ea7db3228c69783bfa8df57c8c3cbb5ad8e885d4
bharath-doshi <bt9944@gmail.com>
2023-08-24 14:44:49 +0530

    fix: bloc support for FourStateBuilder

--- lib/four_state.builder.dart
+++ lib/four_state.builder.dart
@@ -3,7 +3,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 
 import 'callbacks.dart';
 
-class FourStateBuilder<B extends FourStateBloc<T>, T>
+class FourStateBuilder<B extends BlocBase<FourStates<T>>, T>
     extends BlocBuilder<B, FourStates<T>> {
   FourStateBuilder({
     required SucceedBuilder<T> succeedBuilder,
