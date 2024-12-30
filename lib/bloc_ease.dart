32d3dee133c1220213f685c5c859be09207dffb1
Bharath <bt9944@gmail.com>
2024-12-30 20:33:03 +0530

    code refactor

--- lib/bloc_ease.dart
+++ lib/bloc_ease.dart
@@ -1,8 +1,9 @@
 library bloc_ease;
 
-export 'bloc_ease_state.builder.dart';
-export 'bloc_ease_state.consumer.dart';
-export 'bloc_ease_state.listener.dart';
-export 'bloc_ease_states.dart';
-export 'state_widgets.provider.dart';
-export 'bloc_ease_combined_state_listener.dart';
+export 'src/bloc_ease_state.builder.dart';
+export 'src/bloc_ease_state.consumer.dart';
+export 'src/bloc_ease_state.listener.dart';
+export 'src/bloc_ease_states.dart';
+export 'src/state_widgets.provider.dart';
+export 'src/bloc_ease_combined_state.listener.dart';
+export 'src/bloc_ease_combined_state.builder.dart';
---
+++ lib/src/bloc_ease_state.listener.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/widgets.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import 'bloc_ease_states.dart';
+import 'callbacks.dart';
+
+/// Can be used instead of BlocListener.
+/// Provides success object in typesafe manner.
+class BlocEaseStateListener<B extends BlocBase<BlocEaseState<T>>, T> extends StatefulWidget {
+  const BlocEaseStateListener({
+    super.key,
+    this.succeedListener,
+    this.child,
+    this.initialListener,
+    this.loadingListener,
+    this.failureListener,
+    this.bloc,
+    this.listenWhen,
+    this.shouldRunOnInit = false,
+  });
+
+  final bool shouldRunOnInit;
+  final SuccessListener<T>? succeedListener;
+  final Widget? child;
+  final InitialListener? initialListener;
+  final LoadingListener? loadingListener;
+  final FailureListener? failureListener;
+  final B? bloc;
+  final BlocListenerCondition<BlocEaseState<T>>? listenWhen;
+
+  @override
+  State<BlocEaseStateListener> createState() => BlocEaseStateListenerState<B, T>();
+}
+
+class BlocEaseStateListenerState<B extends BlocBase<BlocEaseState<T>>, T>
+    extends State<BlocEaseStateListener> {
+  @override
+  void initState() {
+    if (!widget.shouldRunOnInit) return;
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      final bloc = widget.bloc ?? context.read<B>();
+      final state = bloc.state;
+      if (state is InitialState<T> && widget.initialListener != null) {
+        widget.initialListener!();
+      } else if (state is LoadingState<T> && widget.loadingListener != null) {
+        widget.loadingListener!(state.progress);
+      } else if (state is FailedState<T> && widget.failureListener != null) {
+        widget.failureListener!(state.failureMessage, state.exceptionObject, state.retryCallback);
+      } else if (state is SucceedState<T> && widget.succeedListener != null) {
+        widget.succeedListener!(state.successObject);
+      }
+    });
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocListener<B, BlocEaseState<T>>(
+      bloc: widget.bloc as B?,
+      listenWhen: widget.listenWhen,
+      listener: (context, state) => state.maybeWhen(
+        orElse: () => null,
+        initialState: widget.initialListener,
+        loadingState: widget.loadingListener,
+        succeedState: widget.succeedListener,
+        failedState: widget.failureListener,
+      ),
+      child: widget.child,
+    );
+  }
+}
