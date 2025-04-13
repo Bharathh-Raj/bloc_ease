2d5f1ed52dd66017fe78ca95d6f1bcc9c7da4057
Bharath <bt9944@gmail.com>
2025-04-13 08:33:00 +0530

    feat: InitialState is cacheable

--- lib/src/state_cache.mixin.dart
+++ lib/src/state_cache.mixin.dart
@@ -4,8 +4,8 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 
 import 'states.dart';
 
-/// Caches the last state of [LoadingState], [SucceedState] & [FailedState].
-/// Can access the ex states via [exLoadingState], [exSucceedState] & [exFailedState].
+/// Caches the last state of [InitialState], [LoadingState], [SucceedState] & [FailedState].
+/// Can access the ex states via [exInitialState], [exLoadingState], [exSucceedState] & [exFailedState].
 ///
 /// Automatically destroys the ex states when bloc/cubit closes. Yet we can force clear the ex states by calling [resetCache] method.
 ///
@@ -13,9 +13,13 @@ import 'states.dart';
 /// eg: Animate between [LoadingState] with [LoadingState.progress]
 /// Do operation only on some update in [SucceedState]
 mixin CacheExBlocEaseStateMixin<T> on BlocBase<BlocEaseState<T>> {
+  InitialState<T>? _exInitialState;
   LoadingState<T>? _exLoadingState;
   SucceedState<T>? _exSucceedState;
   FailedState<T>? _exFailedState;
+
+  /// Last [InitialState] of the bloc/cubit.
+  InitialState<T>? get exInitialState => _exInitialState;
 
   /// Last [LoadingState] of the bloc/cubit.
   LoadingState<T>? get exLoadingState => _exLoadingState;
@@ -26,14 +30,14 @@ mixin CacheExBlocEaseStateMixin<T> on BlocBase<BlocEaseState<T>> {
   /// Last [FailedState] of the bloc/cubit.
   FailedState<T>? get exFailedState => _exFailedState;
 
-  /// Last [LoadingState] of the bloc/cubit.
+  /// Last [SucceedState] object of the bloc/cubit.
   T? get exSucceedObject => _exSucceedState?.success;
 
   @override
   void onChange(Change<BlocEaseState<T>> change) {
     final exState = change.currentState;
-    exState.maybeMap(
-      orElse: () => null,
+    exState.map(
+      initialState: (initialState) => _exInitialState = initialState,
       succeedState: (succeedState) => _exSucceedState = succeedState,
       loadingState: (loadingState) => _exLoadingState = loadingState,
       failedState: (failedState) => _exFailedState = failedState,
@@ -42,6 +46,7 @@ mixin CacheExBlocEaseStateMixin<T> on BlocBase<BlocEaseState<T>> {
   }
 
   void resetCache() {
+    _exInitialState = null;
     _exLoadingState = null;
     _exSucceedState = null;
     _exFailedState = null;
